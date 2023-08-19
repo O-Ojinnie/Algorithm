ba1845c7e4c4fc039d2c7db92e14380878460167
O-Ojinnie <74864876+O-Ojinnie@users.noreply.github.com>
2023-08-20 00:02:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 132 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -9,10 +9,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
-		String strX = bf.readLine();
-		String strY = bf.readLine();
-		int X = Integer.parseInt(strX);
-		int Y = Integer.parseInt(strY);
+		int X = Integer.parseInt(bf.readLine().trim());
+		int Y = Integer.parseInt(bf.readLine().trim());
 
 		System.out.println((X > base) && (Y > base) ? 1 : (X > base) && (Y < base) ? 4 : (Y > base ? 2 : 3));
 
